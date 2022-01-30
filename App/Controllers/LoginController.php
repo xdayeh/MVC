df8b94d9256d19be1840dc1943a5844713953190
Mohammad AbuDayeh <e@Hotmail.com>
2022-01-30 14:34:15 +0200

    Add login feature

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -12,9 +12,7 @@ class LoginController extends Controller
 {
     public function login(Request $request, Response $response)
     {
-        if (!Application::isGuest()){
-            $response->redirect();
-        }
+
         $this->setLayout('auth');
         $user = new LoginModel();
         if ($request->isPost()){
